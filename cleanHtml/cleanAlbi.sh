a1709b5f5b7f2cb83d1c510625fd039e615b97ee
Andrea Borruso <aborruso@gmail.com>
2020-01-11 17:20:31 +0100

    update

--- cleanHtml/cleanAlbi.sh
+++ cleanHtml/cleanAlbi.sh
@@ -17,3 +17,5 @@ while IFS=$'\t' read -r cf categoria urlToClean tidy scrape xpath; do
     curl --max-time 60 -k -sL "$urlToClean" -o "$folder"/webpages/"$cf".html -D "$folder"/webpages/"$cf"_code -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36" -H "Sec-Fetch-User: ?1" --compressed
   fi
 done <"$folder"/config.tsv
+
+git add . && git commit -am "update" && git push
